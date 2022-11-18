255f6369aeb0b59f8e36a098515ee0e0a587ae0d
LETSNOTESV7 <a@ex.com>
2022-11-18 15:24:44 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # mistodon-sita2-get-notification
+
+こちらはGoogle Cloud Functionにデプロイされる、5分おきにsita@mistodon.cloudにアクセスし、通知を取得してDBに登録する関数です。
+
+DBへの通知の登録をトリガーとして[reply](https://github.com/raito417/mistodon-sita2-reply)が発火し、メンションへの返信を行います。
---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+import datetime
+from mastodon import Mastodon
+from google.cloud import firestore
+
+JST = datetime.timezone(datetime.timedelta(hours=+9), "JST")
+GCP_PROJECT_ID = os.environ['GCP_PROJECT_ID']
+db = firestore.Client(project=GCP_PROJECT_ID)
+
+# mastodon
+CLIENT_ID = os.environ['CLIENT_ID']
+CLIENT_SECRET = os.environ['CLIENT_SECRET']
+ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+mastodon = Mastodon(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    access_token = ACCESS_TOKEN,
+    api_base_url = 'https://mistodon.cloud'
+)
+
+def get_notification(event, context):
+    mentions = []
+    for n in mastodon.notifications(mentions_only=True):
+        if n["type"] == "mention":
+            mentions.append(n)
+
+    for notification in mentions:
+        id = notification["id"]
+        doc_ref = db.collection("mist_sita/@invocation/notification").document(str(id))
+        doc = doc_ref.get()
+        if not doc.exists:
+            doc_ref.set({
+                "id" : id,
+                "status" : "registered"
+            })
+
+if __name__ =="__main__":
+    get_notification(None, None)
